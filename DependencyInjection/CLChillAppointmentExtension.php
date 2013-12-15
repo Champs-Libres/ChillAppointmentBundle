6c63a2884d7c356db4d5586b95c5b74b2284c379
Julien Fastré <julien@fastre.info>
2013-12-15 20:46:56 +0100

    register to AsseticBundle & add gumby layout to forms

--- DependencyInjection/CLChillAppointmentExtension.php
+++ DependencyInjection/CLChillAppointmentExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class CLChillAppointmentExtension extends Extension
+class CLChillAppointmentExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -25,4 +26,25 @@ class CLChillAppointmentExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
+
+    public function prepend(ContainerBuilder $container) {
+        
+        //register AppointmentBundle into assetic
+        // get all Bundles
+        $bundles = $container->getParameter('kernel.bundles');
+        
+        if (!isset($bundles['AsseticBundle'])) {
+            throw new Exception("You must use Assetic Bundle");
+        }
+        
+        $asseticConfig = $container->getExtensionConfig('assetic');
+        //die(var_dump($asseticConfig));
+        
+        array_push($asseticConfig[0]['bundles'], 'CLChillAppointmentBundle');
+        
+        $config['bundles'] = $asseticConfig[0]['bundles'];
+        
+        $container->prependExtensionConfig('assetic', $config);
+    }
+
 }
